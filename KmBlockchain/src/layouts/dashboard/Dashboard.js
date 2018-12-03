7e1ec04e08c134855261775d24fd0b55db0ecaf3
Juan Castellon <jcc.utn@gmail.com>
2018-12-02 20:49:21 -0600

    KM UI changes

--- KmBlockchain/src/layouts/dashboard/Dashboard.js
+++ KmBlockchain/src/layouts/dashboard/Dashboard.js
@@ -11,8 +11,17 @@ class Dashboard extends Component {
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name} from  {this.props.authData.country}!</strong> If you're seeing this page, you've logged in with UPort successfully.</p>
+            <h1>KmBlockchain Platform Dashboard</h1>
+            <h2>Account Public Key</h2>
+            <p>{this.props.authData.publicEncKey}</p>
+            <h2>DID</h2>
+            <p>{this.props.authData.did}</p>
+            <h2>Name</h2>
+            <p>{this.props.authData.name}</p>
+            <h2>Companies list</h2>
+            <p><strong>BC1</strong>:Token1, Token2, Token3</p>
+            <p><strong>BC2</strong>:Token1, Token2</p>
+            <p><strong>BC4</strong>:Token9, Token4, Token2</p>
           </div>
         </div>
       </main>
