2770969d8c822c4e7369d66ee35cf1f6c1f23680
Juan Castellon <jcc.utn@gmail.com>
2019-01-05 19:27:15 -0600

    Forms added with events

--- truffle-react/client/src/App.js
+++ truffle-react/client/src/App.js
@@ -6,9 +6,31 @@ import truffleContract from "truffle-contract";
 import "./App.css";
 
 class App extends Component {
+
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      companyName: 'some name',
+      phone: '123456789',
+      url: 'www.wearoft.com',
+      did: 'did:0xC123DAB13...',
+      ethadd: '0xD6aE8250b8348C94847280928c79fb3b63cA453e',
+      tokenName: 'Tokenzito',
+      symbol: 'TKN',
+      totalSupply: '1000'
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+
+    //this.handleSubmitCompany = this.handleSubmitCompany.bind(this);
+    //this.handleSubmitToken = this.handleSubmitToken.bind(this);
+
+  }
+  
   componentDidMount = async () => {
+    
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
@@ -23,7 +45,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }/*, this.runExample*/);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -38,20 +60,24 @@ class App extends Component {
 
     // Stores a given value, 5 by default.
     //await contract.createBCCompany("Company Name", "123456789", "44446666", "www.wearoft.com", "did:eth:0xC123D...", "0xD6aE8250b8348C94847280928c79fb3b63cA453e", { from: accounts[0] });
+    var result;
+    //result = await contract.createBCCompany.call("Company Name 1", "44446666", "www.wearoft.com", "did:eth:0xC123D...", "0xD6aE8250b8348C94847280928c79fb3b63cA453e", { from: accounts[0]});
+    //console.log(result);
 
-    var resultBC = await contract.createBCCompany.call("Company Name 1", "44446666", "www.wearoft.com", "did:eth:0xC123D...", "0xD6aE8250b8348C94847280928c79fb3b63cA453e", { from: accounts[0] , gas: 200000000});
-    console.log(resultBC);
+    result = await contract.createBCCompany.call("Company Name 2", "44446666", "www.wearoft.com", "did:eth:0xC123D...", "0xD6aE8250b8348C94847280928c79fb3b63cA453e", { from: accounts[0]});
+    console.log(result);
 
-    var resultBC2 = await contract.createBCCompany.call("Company Name 2", "44446666", "www.wearoft.com", "did:eth:0xC123D...", "0xD6aE8250b8348C94847280928c79fb3b63cA453e", { from: accounts[0], gas: 200000000});
-    console.log(resultBC2);
+    try {
+        result = await contract.findBCownerUtil.call(result, { from: accounts[0]});
+        console.log(result);
+    } catch (err){
+        //console.log(err);
+    }
+   
 
+    result = await contract.returnTrue.call({ from: accounts[0], gas:2000000});
+    console.log(result);
 
-    /*var resultado = await contract.findBCownerUtil(
-      resultBC,
-      { from: accounts[0] }
-    );
-
-    console.log(resultado);*/
     // console.log(resultado);
     //function createBCCompany(string memory _companyName, string memory _phone, string memory _url, string memory _did, address _uPortAddress)
 
@@ -62,23 +88,71 @@ class App extends Component {
     //this.setState({ storageValue: response.toNumber() });
   };
 
+  
+  inputChangeHandler = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmitCompany = async (event) => {
+    event.preventDefault();
+    const { accounts, contract, companyName, phone, url, did, ethadd } = this.state;
+      var result;
+    
+    result = await contract.createBCCompany.call(companyName, phone, url, did, ethadd, { from: accounts[0]});
+    console.log(result);
+
+    try {
+        result = await contract.findBCownerUtil.call(result, { from: accounts[0]});
+        console.log(result);
+    } catch (err){
+        //console.log(err);
+    }
+   
+
+    result = await contract.returnTrue.call({ from: accounts[0], gas:2000000});
+    console.log(result);
+
+  };
+
+  handleSubmitToken= (event) =>  {
+    //alert('A name was submitted: ' + this.state.value);
+    alert('TokenName: ' + this.state.tokenName+
+          '\nSymbol: ' + this.state.symbol+
+          '\nSupply: ' + this.state.totalSupply);
+    //this.runExample();
+    event.preventDefault();
+  };
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="App"> 
+        <h2>Create Company</h2>
+         <form onSubmit={this.handleSubmitCompany}>
+          <label>
+            <span>Company name: <input type="text" id='companyName' defaultValue={this.state.companyName} onChange={this.inputChangeHandler} /></span>
+            Phone: <input type="text" id='phone' defaultValue={this.state.phone} onChange={this.inputChangeHandler} />
+            URL: <input type="text" id='url' defaultValue={this.state.url} onChange={this.inputChangeHandler} />
+            DID: <input type="text" id='did' defaultValue={this.state.did} onChange={this.inputChangeHandler} />
+            Eth add: <input type="text" id='ethadd' defaultValue={this.state.ethadd} onChange={this.inputChangeHandler}  />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h2>Create Token</h2>
+         <form onSubmit={this.handleSubmitToken}>
+          <label>
+            Token name:
+            <input type="text" id='tokenName' defaultValue={this.state.tokenName} onChange={this.inputChangeHandler} />
+            Symbol:
+            <input type="text" id='symbol' defaultValue={this.state.symbol} onChange={this.inputChangeHandler} />
+            Total supply:
+            <input type="text" id='totalSupply' defaultValue={this.state.totalSupply} onChange={this.inputChangeHandler} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
       </div>
     );
   }
