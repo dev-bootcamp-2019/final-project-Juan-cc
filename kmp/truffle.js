40ad96ea73bcafad6576fd9458d8b292aa2f2b38
Juan Castellon <jcc.utn@gmail.com>
2019-01-16 01:07:36 -0600

    rinkeby deployment added

--- kmp/truffle.js
+++ kmp/truffle.js
@@ -1,3 +1,7 @@
+var infura = "https://rinkeby.infura.io/v3/6125978bbbb14d4f968db4ae027bb2ed"
+var HDWalletProvider = require("truffle-hdwallet-provider")
+var mnemonic = "script viable circle vessel argue doll page clay practice toddler fiction attitude";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +11,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, infura),
+      network_id: "4",
+      gas: 6995427
     }
   }
 };
